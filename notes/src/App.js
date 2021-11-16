4a3213cda366de9e3c58a990d25bde73753afd40
CarolinaCasas <casasc9@uabc.edu.mx>
2021-11-15 18:13:56 -0800

    bannerv1

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,4 +1,68 @@
-import logo from './logo.svg';
+import catLogo from './img/catLogo.png';
+import exit from './img/exit.png';
+import './App.css';
+//Aqui van las rutas
+ 
+//Aqui se va a escribir la nota
+// Saca info que llega a NOTE la sube a firestore
+function NewNote(){
+  return  (
+   
+    <div className="newNote"><textarea></textarea>
+    <button>Guardar</button>
+ 
+    </div>
+    );
+}
+ 
+ 
+//Esta va a ser la funcion que imprime las notas
+//Trae la info que saca NewNote la jala desde firestore
+function Note(){
+return(
+<div className="postIt">
+ <h1>Aqui vala nota</h1>
+</div>
+)
+ 
+ 
+}
+ 
+function OpenNoteButton(){
+  const NoteVisible = () =>{console.log("pues si jala")}
+ 
+ 
+  return(<button className="buttonNote"
+  onClick={NoteVisible}>
+    Agregar nota</button>)
+ 
+}
+ 
+function App(props){ 
+return (
+  <div className="App">
+   
+    <header className="header">
+   
+    <div className="CatandLetters">
+    <img src={catLogo} className="catLogo" alt="catLogo"/>
+    <h1 className="hiNoteTittle">Hi!Note</h1>
+    </div>
+    <img src={exit} className="exitImg" alt="exitImg"/>
+    </header>
+   
+   
+    <Note/>
+ <OpenNoteButton/>
+  </div>
+);
+}
+
+export default App;
+
+
+
+/* import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -23,3 +87,4 @@ function App() {
 }
 
 export default App;
+ */
