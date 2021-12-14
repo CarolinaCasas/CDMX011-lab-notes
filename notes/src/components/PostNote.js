212c9af0ffed05ef2dcee0cdd5da020dc38e9c94
CarolinaCasas <casasc9@uabc.edu.mx>
2021-12-14 17:35:52 -0600

    refactorCode

--- notes/src/components/PostNote.js
+++ notes/src/components/PostNote.js
@@ -1,18 +1,30 @@
-import './Modal.css'
+import './note.css'
 import React, { useEffect, useState } from "react";
-import { collection, onSnapshot } from "firebase/firestore";
+import { collection, onSnapshot, doc, deleteDoc} from "firebase/firestore";
 import db from "../firebase/firebaseInital";
+import edit from '../img/edit.png';
+import deleteImg from '../img/delete.png';
+
+
+
 
 function PostNote() {
     const [notes, setNote] = useState([]);
-    useEffect(() => {
 
-        onSnapshot(collection(db, "notes"), (doc) => {
-            
+    const DeleteNote =async(id)=>{
+        const userDoc=  doc(db, 'notes', id);
+        await deleteDoc(userDoc);
+        console.log(id);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onSnapshot(collection(db, "notes"), (doc) => {
             setNote(doc.docs.map((doc) =>({...doc.data(), id: doc.id})));
         });
-    });
 
+        return unsubscribe;
+    }, []);
+ 
 
     return (
         <div className="postNoteContainer">
@@ -20,6 +32,10 @@ function PostNote() {
                 <div className="postNote" key={note.id}>
                     <p className="titleNotes">{note.titleNote}</p>
                     <p>{note.bodyNote}</p>
+                    <div className="buttons">
+                    <img src={edit} className="editImg" alt="editImg" />
+                    <img src={deleteImg} className="deleteImg" alt="deleteImg" onClick={()=>{DeleteNote(note.id)}}/>
+                    </div>
                 </div>
             )
              )}
