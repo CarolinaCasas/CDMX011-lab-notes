e9de828d2c7184d66fbf7bb5884de4384f0411d6
CarolinaCasas <casasc9@uabc.edu.mx>
2021-12-15 14:50:40 -0600

    completeRoutes

--- notes/src/routesComp/register.js
+++ notes/src/routesComp/register.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Register(){
 return(
     <div>
         <h1>Register</h1>
+        <Link to="/home" className="Login"><button>Registrarse</button></Link>
+       
+
+        <h2>¿Ya tienes una cuenta?</h2>
+            <h2>Inicia sesión</h2>
+            <Link to="/" className="Login">aquí</Link>
     </div>
 )
 }
