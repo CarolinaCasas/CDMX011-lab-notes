8eea0bbce706ce6a63fcd5fcde484244779852a4
CarolinaCasas <casasc9@uabc.edu.mx>
2021-12-24 01:04:29 -0600

    cssLogin

--- notes/src/routesComp/login.js
+++ notes/src/routesComp/login.js
@@ -1,23 +1,26 @@
 import { Link } from 'react-router-dom';
-
+import '../App.css';
 
 function Login() {
   return (
     <div>
-      <h1>Hi!note</h1>
-
-      <form>
-        <input placeholder='Ingresa correo' /><br/>
-        <input placeholder='Ingresa contraseña' /><br/>
-        <Link to="/home" className="Login"><button>Entrar</button></Link>
-      </form>
-
+      <h1 className='hiNote'>Hi!note</h1>
+      <div className='blueContainer'>
+        <form div className='loginform'>
+          <input className="loginInput" placeholder='Ingresa correo' /><br />
+          <input className="loginInput" placeholder='Ingresa contraseña' /><br />
+          <Link to="/home" className="Login"><button className='send'>Entrar</button></Link>
+        </form>
+        <hr className='lineaDeSeccion'/>
+        <button className='google'>Iniciar sesión con Google</button>
+      </div>
       <section>
-        <h2 className='registro'>¿Aún no tienes una cuenta?<br/>
-         Registrate <Link to="/register" className="register">aquí</Link></h2>
-        <hr/>
-        <h2>Iniciar sesión con Google</h2>
+        <h2 className='registro'>¿Aún no tienes una cuenta?<br />
+          Registrate <Link to="/register" className="register">aquí</Link></h2>
+       
+
       </section>
+
     </div>)
 
 }
