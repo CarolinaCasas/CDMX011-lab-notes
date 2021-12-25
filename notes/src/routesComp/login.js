0c74807e08975bd85349c8412fd5a7d4f078c9ea
CarolinaCasas <casasc9@uabc.edu.mx>
2021-12-25 12:58:31 -0600

    ipadLoginVisual

--- notes/src/routesComp/login.js
+++ notes/src/routesComp/login.js
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom';
+import blackcat from '../img/blackcat.png';
+import googleLogo from '../img/googleLogo.png';
 import '../App.css';
 
 function Login() {
   return (
-    <div>
+    <div className='loginSection'>
       <h1 className='hiNote'>Hi!note</h1>
       <div className='blueContainer'>
         <form div className='loginform'>
@@ -12,15 +14,12 @@ function Login() {
           <Link to="/home" className="Login"><button className='send'>Entrar</button></Link>
         </form>
         <hr className='lineaDeSeccion'/>
-        <button className='google'>Iniciar sesión con Google</button>
+        <button className='google'><img src={googleLogo} className="googleLogo" alt="googleLogo" />Iniciar sesión con Google</button>
       </div>
-      <section>
+      <img src={blackcat} className="frontCatImg" alt="frontCatImg" />
         <h2 className='registro'>¿Aún no tienes una cuenta?<br />
-          Registrate <Link to="/register" className="register">aquí</Link></h2>
-       
-
-      </section>
-
+          Registrate <Link to="/register" className="register">Aquí</Link></h2>
+          
     </div>)
 
 }
